9f306f1b4c3262488b2860ea5bd0b1d8be189076
Flo <florian.steck@sap.com>
2025-05-12 13:30:33 +0200

    implemented first card for profile

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,13 +1,19 @@
 <template>
-  <div>
-    
-  </div>
+  <UCard class="Card">
+    <template #header>
+      <UAvatar src="/images/profilpic.png" />
+    </template>
+
+    <Placeholder class="h-32" />
+
+    <template #footer>
+      <Placeholder class="h-8" />
+    </template>
+  </UCard>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
 
 <style>
-
+@import url("~/assets/css/login.css");
 </style>
