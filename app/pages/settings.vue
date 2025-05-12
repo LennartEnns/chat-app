c5b0afb7a877336eeaa62fb033f3f2a3c5be2d7a
JW884 <prowltop@gmail.com>
2025-05-12 15:19:18 +0200

    added password change page and theme selection

---
+++ app/pages/settings.vue
@@ -0,0 +1,112 @@
+<template>
+  <UApp class="flex align-content-center justify-center">
+    <div class="mainContainer">
+      <URadioGroup v-model="value" :items="items" variant="card" />
+
+      <UCollapsible class="flex flex-col gap-2 w-48">
+        <UButton
+          class="group"
+          label="Open"
+          color="neutral"
+          variant="subtle"
+          trailing-icon="i-lucide-chevron-down"
+          :ui="{
+            trailingIcon:
+              'group-data-[state=open]:rotate-180 transition-transform duration-200',
+          }"
+          block
+        />
+        <UFormField
+          label="Change password"
+          description="enter your old password to continue"
+          size="lg"
+        ></UFormField>
+
+        <template #content>
+          <UInput
+            v-model="passwordOld"
+            placeholder="Old password"
+            :type="showOld ? 'text' : 'password'"
+            :ui="{ trailing: 'pe-1' }"
+          >
+            <template #trailing>
+              <UButton
+                color="neutral"
+                variant="link"
+                size="sm"
+                :icon="showOld ? 'i-lucide-eye-off' : 'i-lucide-eye'"
+                :aria-label="showOld ? 'Hide password' : 'Show password'"
+                :aria-pressed="showOld"
+                aria-controls="password"
+                @click="showOld = !showOld"
+              />
+            </template>
+          </UInput>
+
+          <UInput
+            v-model="passwordNew"
+            placeholder="New password"
+            :type="showNew ? 'text' : 'password'"
+            :ui="{ trailing: 'pe-1' }"
+          >
+            <template #trailing>
+              <UButton
+                color="neutral"
+                variant="link"
+                size="sm"
+                :icon="showNew ? 'i-lucide-eye-off' : 'i-lucide-eye'"
+                :aria-label="showNew ? 'Hide password' : 'Show password'"
+                :aria-pressed="showNew"
+                aria-controls="password"
+                @click="showNew = !showNew"
+              />
+            </template>
+          </UInput>
+
+          <UInput
+            v-model="passwordNewCheck"
+            placeholder="Re-enter new password"
+            :type="showNewCheck ? 'text' : 'password'"
+            :ui="{ trailing: 'pe-1' }"
+          >
+            <template #trailing>
+              <UButton
+                color="neutral"
+                variant="link"
+                size="sm"
+                :icon="showNewCheck ? 'i-lucide-eye-off' : 'i-lucide-eye'"
+                :aria-label="showNewCheck ? 'Hide password' : 'Show password'"
+                :aria-pressed="showNewCheck"
+                aria-controls="password"
+                @click="showNewCheck = !showNewCheck"
+              />
+            </template>
+          </UInput>
+        </template>
+      </UCollapsible>
+    </div>
+  </UApp>
+</template>
+
+<script lang="ts" setup>
+import RegistrationForm from "~/components/Form/RegistrationForm.vue";
+// import UFormField from "~/components/Form/FormField.vue";
+import type { RadioGroupItem, RadioGroupValue } from "@nuxt/ui";
+
+const items = ref<RadioGroupItem[]>(["System", "Light", "Dark"]);
+const value = ref<RadioGroupValue>("System");
+
+const showOld = ref(false);
+
+const showNew = ref(false);
+const showNewCheck = ref(false);
+const passwordOld = ref("");
+const passwordNew = ref("");
+const passwordNewCheck = ref("");
+</script>
+
+<style>
+.mainContainer {
+  width: 90%;
+}
+</style>
