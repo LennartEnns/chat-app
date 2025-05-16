2fa534f8a11ac440a2a6268f2da971d448b31412
Lennart Enns <lennart.enns@sap.com>
2025-05-16 10:16:19 +0200

    Email redirect: Workaround

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -5,7 +5,11 @@
 </template>
 
 <script lang="ts" setup>
-  import LoginForm from '~/components/Form/LoginForm.vue';
+import LoginForm from '~/components/Form/LoginForm.vue';
+
+// Workaround: Ignore code because exchangeCodeForSession does not work
+const route = useRoute()
+if (route.query.code) navigateTo("/login");
 </script>
 
 <style>
