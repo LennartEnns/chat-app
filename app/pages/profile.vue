b3c396080c8b5656deddbd9a261d6eabcec37cb1
JW884 <prowltop@gmail.com>
2025-05-19 15:19:34 +0200

    reduced css

--- app/pages/profile.vue
+++ app/pages/profile.vue
@@ -25,11 +25,9 @@
                 type="file"
                 style="
                   position: absolute;
-                  top: 0;
-                  left: 0;
                   width: 100%;
                   height: 100%;
-                  opacity: 0;
+                  color: transparent;
                 "
                 accept="image/*"
                 @change="uploadPic"
