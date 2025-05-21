a9419813a06fb3a3acfeebc93f849c5961f5b39a
Flo <florian.steck@sap.com>
2025-05-21 09:42:06 +0200

    added border line to seperate chats from contactlist

--- app/pages/chat.vue
+++ app/pages/chat.vue
@@ -4,7 +4,7 @@
       <!--Mobile UI drawer for choosing chats-->
       <UDrawer v-model:open="drawerOpen" direction="bottom" v-if="isMobile">
         <template #body>
-          <div class="align-column">
+          <div class="align-column contactList">
             <UModal v-model:open="open" class="mb-[10px]">
               <UButton
                 label="Search users..."
