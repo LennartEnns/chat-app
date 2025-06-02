41d489a8d55e3ce9230a252cf679c740b8f513bc
JW884 <prowltop@gmail.com>
2025-06-02 10:23:29 +0200

    fixed too small container when. viewing user profile

--- app/pages/profile/[username].vue
+++ app/pages/profile/[username].vue
@@ -1,7 +1,7 @@
 <template>
   <NuxtLayout name="logged-in">
     <div class="flex justify-center align-middle">
-      <div class="glassContainer max-w-[70%]">
+      <div class="glassContainer w-[35rem] max-w-[70%]">
         <UModal
           v-model:open="showAvatarCroppingModal"
           title="Crop Avatar"
