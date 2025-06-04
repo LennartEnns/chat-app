f92e11e2abfc2be7e0aaf7971e225ef9ad8d3a2d
Jakob Brodel <jakob.brodel@sap.com>
2025-06-04 14:09:35 +0200

    added responsive design

--- app/pages/chatInfo.vue
+++ app/pages/chatInfo.vue
@@ -1,6 +1,6 @@
 <template>
   <NuxtLayout name="logged-in">
-    <div class="grid grid-cols-4 h-full">
+    <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 h-full">
       <div class="flex flex-col items-center p-5">
         <div class="pt-10 pb-5">
           <div class="max-w-fit">
@@ -18,7 +18,7 @@
         </div>
       </div>
       <div
-        class="flex flex-col items-center col-span-2 px-5 relative border border-defaultNeutral-700 border-t-0 border-b-0"
+        class="flex flex-col items-center col-span-2 px-5 relative md:border border-defaultNeutral-700 md:border-t-0 md:border-b-0 md:border-r-0 lg:border-t-0 lg:border-b-0 lg:border-r"
       >
         <div class="pb-5 text-neutral-700 dark:text-white">
           <p>Members</p>
@@ -40,14 +40,52 @@
             </div>
           </div>
         </div>
-        <UButton
-          size="xl"
-          icon="i-lucide-plus"
-          class="absolute bottom-5 right-5"
-          >Add member</UButton
-        >
+        <div class="absolute bottom-5 right-5 flex flex-col">
+          <div class="flex flex-row justify-end">
+            <div class="hidden md:block lg:hidden">
+              <UButton
+                class="flex size-fit mb-3"
+                @click="openDrawer"
+                color="primary"
+                trailing-icon="i-lucide-chevron-left"
+              />
+            </div>
+          </div>
+          <UButton size="xl" icon="i-lucide-plus">Add member</UButton>
+        </div>
       </div>
-      <div class="flex flex-col items-center px-5">
+      <UDrawer
+        direction="right"
+        v-model:open="open"
+        class="hidden md:block lg:hidden"
+      >
+        <template #body>
+          <div class="flex flex-col items-center px-5">
+            <div class="pb-5 text-neutral-700 dark:text-white">
+              <p>Invitations</p>
+            </div>
+            <div class="pb-5">
+              <div
+                class="ring-0 glassContainer text-neutral-700 dark:text-white member invitation"
+              >
+                <UAvatar
+                  class="justify-self-center"
+                  src="https://github.com/nuxt.png"
+                />
+                <div
+                  class="flex flex-col items-center justify-center truncate px-[0.6rem]"
+                >
+                  <div class="truncate w-full text-center">
+                    Peterskotstube wrkegjtlwkjertlwkejrce
+                  </div>
+                </div>
+                <UButton icon="i-lucide-trash-2" class="size-fit"></UButton>
+              </div>
+            </div>
+          </div>
+        </template>
+      </UDrawer>
+      <div class="flex flex-col items-center px-5 lg:block md:hidden">
         <div class="pb-5 text-neutral-700 dark:text-white">
           <p>Invitations</p>
         </div>
@@ -74,7 +112,13 @@
   </NuxtLayout>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const open = ref(false);
+
+async function openDrawer() {
+  open.value = true;
+}
+</script>
 
 <style>
 @import url("~/assets/css/chatInfo.css");
