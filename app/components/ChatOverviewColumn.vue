9c61ab27335ecd1e962b3a24102f6475f8321008
JW884 <prowltop@gmail.com>
2025-06-05 11:15:07 +0200

    implemented existing chats or messages list

--- app/components/ChatOverviewColumn.vue
+++ app/components/ChatOverviewColumn.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="align-column">
-    <ModalSearchUser @close="onUserSelect">
+    <ModalSearchUser class="mb-2" @close="onUserSelect">
       <UButton
         label="Search Users"
         color="neutral"
@@ -10,12 +10,19 @@
       />
     </ModalSearchUser>
     <UButton
-      class="mb-[10px]"
       label="Create Chatroom"
+      class="mb-4"
       color="primary"
       variant="solid"
       icon="i-lucide-plus"
       @click="onCreateChat"
+    />
+    <UButton
+      v-for="(user, i) in availableChatrooms.data"
+      class="mb-1"
+      :label="user.name"
+      variant="subtle"
+      @click="openChat(user.id)"
     />
   </div>
 </template>
@@ -37,8 +44,17 @@ async function onCreateChat() {
   // Open without awaiting a result, as creation logic is handled inside the Modal
   createChatroomModal.open();
 }
+
+const supabase = useSupabaseClient();
+let availableChatrooms: Object = await supabase.from("chatrooms").select("*");
+availableChatrooms.data?.forEach((element: Object, i: Number) => {
+  console.log(element);
+  console.log(i);
+});
+
+async function openChat(id: String) {
+  navigateTo("/chat/" + id);
+}
 </script>
 
-<style>
-
-</style>
+<style></style>
