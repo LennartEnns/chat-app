d5d957da5d949e5c2b730cf6089b285b826527b6
JW884 <prowltop@gmail.com>
2025-06-10 14:40:52 +0200

    cleaned up chat index.vue

--- app/pages/chat/index.vue
+++ app/pages/chat/index.vue
@@ -6,11 +6,6 @@
         <h1 class="headlineChat-desktop">You didn't open any chats yet</h1>
         <USeparator :class="`${themedSeparator}`" color="primary" />
       </div>
-      <ChatroomPreview
-        v-for="chatroom in chatroomsWithAvatarUrl"
-        :name="chatroom.name"
-        :avatar-url="chatroom.avatarUrl"
-      ></ChatroomPreview>
 
       <!-- Container for Mobile -->
       <div v-if="isMobile">
@@ -101,40 +96,11 @@
 <script lang="ts" setup>
 import { logPostgrestError } from "~~/errors/postgrestErrors";
 
-export interface UserData {
-  user_id: string;
-  username: string;
-  displayname: string | null;
-  description: string | null;
-  avatarPath: string;
-  avatarUrl: string;
-}
-
-export interface DirectChatroomData {
-  chatroom_id: string;
-  new_messages: number;
-  user1: UserData;
-  user2: UserData;
-  loggedUser: UserData;
-}
-
-export interface GroupChatroomData {
-  chatroom_id: string;
-  name: string;
-  new_messages: number;
-  users: UserData[];
-}
-
 const isMobile = useMobileDetector();
 const { isLight } = useSSRSafeTheme();
 const supabase = useSupabaseClient();
-const { data } = await supabase.auth.getSession();
-const sessionData: string | undefined = data.session?.user.updated_at;
-const convertedSessionData = sessionData ? new Date(sessionData) : null;
-
 const userData = useUserData();
-const directChatrooms = ref<DirectChatroomData[]>([]);
-const groupChatrooms = ref<GroupChatroomData[]>([]);
+
 const themedGlassContainer = computed(() =>
   isLight.value ? "mainGlassContainer-dark" : "mainGlassContainer-light"
 );
@@ -144,35 +110,6 @@ const themedSeparator = computed(() =>
 const themedSeparatorMobile = computed(() =>
   isLight.value ? "separator-dark-mobile" : "separator-light-mobile"
 );
-
-// Get new messages count
-async function getNewMessagesCount(chatroom_id: string): Promise<number> {
-  const { data, error } = await supabase
-    .from("messages")
-    .select("*")
-    .eq("chatroom_id", chatroom_id);
-  console.log(data);
-  if (error) {
-    logPostgrestError(error, "timestamp fetching");
-    return 0;
-  }
-  if (data === null || data.length === 0) {
-    console.log("No messages found for chatroom_id:" + chatroom_id);
-    return 0;
-  }
-  let messageCount = 0;
-  for (const message of data) {
-    if (convertedSessionData != undefined) {
-      const messageDate = new Date(message.created_at);
-      if (messageDate > convertedSessionData) {
-        messageCount++;
-      }
-    }
-  }
-  if (messageCount > 0) {
-    return messageCount;
-  } else return 0;
-}
 
 function generateAvatarUrl(
   type: string,
@@ -194,30 +131,14 @@ function generateAvatarUrl(
       };
 }
 
-async function getGroupChatroomName(chatroom_id: string): Promise<string> {
-  const { data, error } = await supabase
-    .from("group_chatrooms")
-    .select("name")
-    .eq("chatroom_id", chatroom_id);
-  if (error) {
-    logPostgrestError(error, "No chatroom name");
-    return "Unnamed Group";
-  }
-  if (!data || data.length === 0) {
-    console.log("No data found");
-    return "Unnamed Group";
-  }
-  return data[0]?.name || "Unnamed Group";
-}
-
 const previewQuery = supabase
   .from("chatrooms_preview")
   .select("*")
   .order("last_activity", { ascending: false });
 
-const chatroomList = ref<Awaited<typeof previewQuery>["data"]>([]);
-
-async function getChatroomList(user_id: string): Promise<UserData | null> {
+async function getChatroomList(
+  user_id: string
+): Promise<Awaited<typeof previewQuery>["data"] | null> {
   const { data, error } = await previewQuery;
 
   if (error) {
@@ -232,10 +153,10 @@ async function getChatroomList(user_id: string): Promise<UserData | null> {
   return data;
 }
 
-// await getChatroomList(userData.id).forEach((element) => {
-//   console.log(element.name);
-// });
-const chatrooms = ref(await getChatroomList(userData.id));
+let chatrooms = ref<Awaited<typeof previewQuery>["data"]>([]);
+const chatroomListResult = await getChatroomList(userData.id);
+chatrooms.value = chatroomListResult ?? [];
+
 const chatroomsWithAvatarUrl = computed(() =>
   chatrooms.value?.map((chatroom) => {
     const { avatarUrl } = generateAvatarUrl(
@@ -249,8 +170,7 @@ const chatroomsWithAvatarUrl = computed(() =>
     };
   })
 );
-
-console.log(chatroomsWithAvatarUrl.value[0].avatarUrl);
+console.log(chatroomsWithAvatarUrl.value);
 </script>
 
 <style>
