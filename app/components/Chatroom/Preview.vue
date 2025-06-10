5d68a8a28df353dd21b40d10400b37e309dc767c
JW884 <prowltop@gmail.com>
2025-06-10 14:41:04 +0200

    added routing to chat list

--- app/components/Chatroom/Preview.vue
+++ app/components/Chatroom/Preview.vue
@@ -1,5 +1,5 @@
 <template>
-  <UButton variant="subtle">
+  <UButton variant="subtle" @click="onUserSelect()">
     <UAvatar :src="avatarUrl" icon="i-lucide-user" size="md" />
     <div class="ml-2">
       <span class="font-bold">{{ name }}</span>
@@ -8,12 +8,19 @@
 </template>
 
 <script lang="ts" setup>
+import type { UserSearchResult } from "~/types/userSearch";
 console.log("loaded");
 const props = defineProps<{
+  id: string;
   name: string;
   avatarUrl: string | undefined;
 }>();
 console.log("Preview component loaded with props:", props.avatarUrl);
+
+// Handle user selection in the command palette
+async function onUserSelect() {
+  navigateTo(`/chat/${props.id}`);
+}
 </script>
 
 <style></style>
