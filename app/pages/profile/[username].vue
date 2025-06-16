e2ca9f32b264ee8628594350b4a489c2fb5ef6af
Lennart Enns <lennart.enns@sap.com>
2025-06-16 20:01:17 +0200

    Linkification of profile description

--- app/pages/profile/[username].vue
+++ app/pages/profile/[username].vue
@@ -159,6 +159,7 @@
                         : ''
                     }`"
                   >
+                    <!-- eslint-disable vue/no-v-html -->
                     <div
                       v-if="!isOwnProfile || !isEditingDescription"
                       :class="`whitespace-pre-line wrap-anywhere ${
@@ -166,13 +167,9 @@
                           ? themedWeakColor
                           : themedTextsColor
                       }`"
-                    >
-                      {{
-                        isFalsy(profileData.description)
-                          ? "Empty"
-                          : profileData.description
-                      }}
-                    </div>
+                      v-html="descriptionLinkified"
+                    />
+                    <!-- eslint-enable -->
                     <UTextarea
                       v-else
                       v-model="newDescription"
@@ -250,6 +247,9 @@ const isEditingDescription = ref(false);
 const newDescription = ref("");
 const descriptionChanged = computed(
   () => userData.description !== newDescription.value
+);
+const descriptionLinkified = useLinkifiedText(
+  computed(() => isFalsy(profileData.value?.description) ? 'Empty' : profileData.value!.description!)
 );
 
 const descriptionRowCount = computed(() =>
