d837ce5486327fd4d50b063251879ffc55bdb3c1
JW884 <prowltop@gmail.com>
2025-06-18 09:52:16 +0200

    Implemented proper checks for updating chatrooms
    only mods or admins can change anything, only pinned msg can be updated

--- supabase/migrations/20250617144850_implement_message_pinning.sql
+++ supabase/migrations/20250617144850_implement_message_pinning.sql
@@ -10,18 +10,26 @@ security definer set search_path = ''
 as $$
 declare 
   role text;
+  old_with_pinned_msg public.chatrooms;
+
 begin
+old_with_pinned_msg := old;
+      old_with_pinned_msg.pinned_message_id := new.pinned_message_id;
+      -- for eunsuring only the pinned_message_id is updated
+
+ if old_with_pinned_msg is distinct from new then
+    raise exception 'only the pinned_message_id can be updated';
+  end if;
   if new.pinned_message_id is distinct from old.pinned_message_id then
     select utils.get_role_in_chatroom((select auth.uid()), new.id) into role;
-    if role ='admin' then
-    return new;
+    if role is null then
+        raise exception 'You are not a member of this chatroom';
     end if;
-    if role = 'mod' then
-      return new;
-    end if;
-    
-  end if;
+    if role not in ('mod','admin') then
       raise exception 'Only mods and admins can pin messages';
+    end if;
+  end if;
+  return new;
 end;
 $$;
 
