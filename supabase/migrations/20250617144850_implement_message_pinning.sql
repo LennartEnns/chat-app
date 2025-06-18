f08b16c62dac96c8bfc00fc850948b18025c4f6c
JW884 <prowltop@gmail.com>
2025-06-18 09:08:29 +0200

    function to limit db changes on chatrooms to mods and admin (currently no differentiation)

--- supabase/migrations/20250617144850_implement_message_pinning.sql
+++ supabase/migrations/20250617144850_implement_message_pinning.sql
@@ -1,5 +1,31 @@
-
--- This creates the circular reference and enforces your business rule.
 alter table public.chatrooms
-  add constraint chatrooms_pinned_message_fkey
-  foreign key (id, pinned_message) references public.messages (chatroom_id, id) on delete set null;
+  add constraint chatrooms_pinned_message_id_fkey
+  foreign key (id, pinned_message_id) references public.messages (chatroom_id, id) on delete set null;
+
+
+create or replace function verify_role_on_message_pinning()
+returns trigger
+language plpgsql
+security definer set search_path = ''
+as $$
+declare 
+  role text;
+begin
+  if new.pinned_message_id is distinct from old.pinned_message_id then
+    select utils.get_role_in_chatroom((select auth.uid()), new.id) into role;
+    if role ='admin' then
+    return new;
+    end if;
+    if role = 'mod' then
+      return new;
+    end if;
+    
+  end if;
+      raise exception 'Only mods and admins can pin messages';
+end;
+$$;
+
+create trigger before_update_verify_role_on_message_pinning
+before update on public.chatrooms
+for each row
+execute function verify_role_on_message_pinning();
